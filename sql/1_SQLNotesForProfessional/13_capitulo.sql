8a6dfbb31b17570940e08a0e6418e763e2637bb6
okluan <luanemerson8@gmail.com>
2024-09-07 12:43:46 -0300

    Alterações

--- sql/1_SQLNotesForProfessional/13_capitulo.sql
+++ sql/1_SQLNotesForProfessional/13_capitulo.sql
@@ -3,7 +3,21 @@ use sqlnotes;
 select concat(Pnome, '_',  Lnome) from datas_1
 
 create table ItemSales (
-id 
-saledata
-quantidade
+id int,
+saledata date,
+quantidade int
+)
+
+insert into ItemSales (id, saledata, quantidade)
+values (1, '2013-07-01', 10),
+(2, '2013-07-01', 15),
+(3, '2013-07-23', 15),
+(4, '2013-07-26', 30)
+
+select * from ItemSales
+where saledata between '2013-06-01' and '2013-07-01'
+and quantidade = 10
+
+
+create table costumerid(
 )
