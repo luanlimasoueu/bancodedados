7ad4a7ce5136edac3352592a8d544b654ce97eab
okluan <luanemerson8@gmail.com>
2024-09-07 00:30:55 -0300

    Alterações

---
+++ sql/1_SQLNotesForProfessional/1_Inicio.sql
@@ -0,0 +1,3 @@
+create database sqlnotes;
+
+use sqlnotes;
---
+++ sql/1_exemplo/1_exemplo.sql
@@ -0,0 +1,11 @@
+delimiter //
+CREATE PROCEDURE citycount (IN country CHAR(3), OUT cities INT)
+       BEGIN
+         SELECT COUNT(*) INTO cities FROM world.city
+         WHERE CountryCode = country;
+END//
+delimiter ;
+
+CALL citycount('JPN', @cities);
+
+SELECT @cities;
