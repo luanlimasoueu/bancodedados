83e92d560547f3a8f2cc46bbadaec09c3e863815
okluan <luanemerson8@gmail.com>
2025-04-19 03:57:52 -0300

    Study

--- sql/12_SQL_Cookbook/2_capitulo/2_1_capitulo.sql
+++ sql/12_SQL_Cookbook/2_capitulo/2_1_capitulo.sql
@@ -27,3 +27,27 @@ from emp
 select * from V
 
 
+select ename,sal,comm
+from emp
+order by 3
+
+
+select ename,sal,comm
+from (
+select ename,sal,comm,
+case when comm is null then 0 else 1 end as is_null
+from emp
+) x
+order by is_null desc,comm
+
+
+select ename,sal,job,comm
+from emp
+order by case when job = 'SALESMAN' then comm else sal end
+
+
+select ename,sal,job,comm,
+case when job = 'SALESMAN' then comm else sal end as ordered
+from emp
+order by 5
+
