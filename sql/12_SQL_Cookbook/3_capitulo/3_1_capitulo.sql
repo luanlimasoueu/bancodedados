c1f519396aa4c5aa2309b41db23ebcdf51d27c7e
okluan <luanemerson8@gmail.com>
2025-04-20 17:47:41 -0300

    Study

---
+++ sql/12_SQL_Cookbook/3_capitulo/3_1_capitulo.sql
@@ -0,0 +1,18 @@
+use cookbook;
+
+select ename as ename_and_dname, deptno
+from emp
+where deptno = 10
+union all
+ select '----------', null
+from t1
+union all
+select dname, deptno
+from dept
+
+
+select deptno
+from emp
+union
+select deptno
+from dept
