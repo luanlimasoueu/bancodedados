0b7888b0919127b53de673675ca0fcde9eda2b7e
okluan <luanemerson8@gmail.com>
2025-04-23 05:25:53 -0300

    Study

--- sql/12_SQL_Cookbook/6_capitulo/6_1_capitulo.sql
+++ sql/12_SQL_Cookbook/6_capitulo/6_1_capitulo.sql
@@ -47,3 +47,73 @@ as stripped1,
 sal,
 replace(sal,0,'') stripped2
 from emp
+
+select data,
+translate(lower(data),
+'abcdefghijklmnopqrstuvwxyz',
+rpad('z',26,'z')) sal
+from (select ename||sal data from emp)
+
+
+select data,
+to_number(
+replace(
+translate(lower(data),
+'abcdefghijklmnopqrstuvwxyz',
+rpad('z',26,'z')),'z')) sal
+from (select ename||sal data from emp)
+
+select data,
+replace(translate(data,'0123456789','0000000000'),'0') ename
+from (select ename||sal data from emp)
+
+create view V_4 as
+select ename as data
+from emp
+where deptno=10
+union all
+select concat(ename,', $',sal,'.00') as data
+from emp
+where deptno=20
+union all
+select concat(ename,deptno) as data
+from emp
+where deptno=30
+
+select * from  V_4
+
+select data
+from V_4
+where translate(lower(data),
+'0123456789abcdefghijklmnopqrstuvwxyz',
+rpad('a',36,'a')) = rpad('a',length(data),'a')
+
+ select case
+ when cnt = 2 then
+ trim(trailing '.' from
+ concat_ws('.',
+ substr(substring_index(name,' ',1),1,1),
+ substr(name,
+length(substring_index(name,' ',1))+2,1),
+substr(substring_index(name,' ',-1),1,1),
+ '.'))
+else
+ trim(trailing '.' from
+concat_ws('.',
+ substr(substring_index(name,' ',1),1,1),
+ substr(substring_index(name,' ',-1),1,1)
+ ))
+end as initials
+from (
+select name,length(name)-length(replace(name,' ','')) as cnt
+from (
+select replace('Stewie Griffin','.','') as name from t1
+
+
+select substr(substring_index(name, ' ',1),1,1) as a,
+substr(substring_index(name,' ',-1),1,1) as b
+from (select 'Stewie Griffin' as name from t1) x
+
+
+
+
