0e3a18a1e32304b5a30221862b9d387ad05686a6
Luan Lima <luanemerson8@gmail.com>
2026-05-24 10:14:13 -0300

    Banco de dados olist

--- olist/olist_script.sql
+++ olist/olist_script.sql
@@ -135,3 +135,16 @@ DELIMITER ',' CSV HEADER;
 
 select * from olist_order_reviews_dataset;
 
+
+--product_category_name_translation 
+--olist_geolocation_dataset 
+--olist_order_customer_dataset 
+--olist_sellers_dataset 
+--olist_products_dataset 
+--olist_orders_dataset 
+--olist_order_items_dataset 
+--olist_order_payments_dataset 
+--olist_order_reviews_dataset 
+
+
+
